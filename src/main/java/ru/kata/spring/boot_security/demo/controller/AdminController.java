e217ff6f0f00f8ee454af5db72552716e561b920
dogute <dogute7@gmail.com>
2022-09-14 22:34:58 +1000

    commit #8

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,59 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/")
+    public String userList(Model model) {
+        model.addAttribute("usersList", userService.getAllUsers());
+        model.addAttribute("rolesList", userService.getAllRoles());
+        model.addAttribute("newUser", new User());
+        model.addAttribute("authorisedUser", (User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal());
+        return "workPage";
+    }
+
+    @PostMapping("/")
+    public String addUser(@ModelAttribute("newUser") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        User userToEdit = userService.getUserById(id);
+        String roleUser = (userToEdit.getStringRoles().contains("ROLE_USER") ? "on" : null);
+        String roleAdmin = (userToEdit.getStringRoles().contains("ROLE_ADMIN") ? "on" : null);
+
+        model.addAttribute("userToEdit", userService.getUserById(id));
+        model.addAttribute("roleUser", roleUser);
+        model.addAttribute("roleAdmin", roleAdmin);
+        return "adminController/edit";
+    }
+
+    @PutMapping("/{id}")  // edit user
+    public String update(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/admin/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin/";
+    }
+
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,91 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.dao.UserDao;
+import ru.kata.spring.boot_security.demo.dao.RoleDao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final UserDao userDao;
+    private final RoleDao roleDao;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserDao userDao, RoleDao roleDao, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.roleDao = roleDao;
+        this.userDao = userDao;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+
+    }
+
+    // в коде явно не вызывается
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
+
+        if (username == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return user;
+    }
+
+    @Transactional
+    public User getUserById(Long id) {
+        Optional<User> userFromDB = userDao.findById(id);
+        return userFromDB.orElse(new User());
+    }
+
+    public List<User> getAllUsers() {
+        return userDao.findAll();
+    }
+
+    @Transactional
+    public boolean saveUser(User user) {
+        if (userDao.findByUsername(user.getFirstName()) != null) {
+            throw new IllegalArgumentException(String.format("User with username %s already exists in database", user.getFirstName()));
+        }
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userDao.save(user);
+        return true;
+    }
+
+    @Transactional
+    public boolean updateUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userDao.save(user);
+        return true;
+    }
+
+    @Transactional
+    public boolean deleteUser(Long id) {
+        if (userDao.findById(id).isPresent()) {
+            userDao.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Transactional
+    public List<Role> getAllRoles() {
+        return roleDao.findAll();
+    }
+
+}
